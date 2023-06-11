6690ebac599770f5f50b166e1596fffb0728da66
Mafadi Lephaka <mafadilephaka97@gmail.com>
2023-06-11 19:27:14 +0200

    Created users app

--- myschoolAPI/myschoolAPI/settings.py
+++ myschoolAPI/myschoolAPI/settings.py
@@ -73,13 +73,26 @@ WSGI_APPLICATION = 'myschoolAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myshooldatabase',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
