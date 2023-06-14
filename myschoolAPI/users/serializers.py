2fa464dd1c4219072cd8f2e280f7204592b505cd
Mafadi Lephaka <mafadilephaka97@gmail.com>
2023-06-14 07:06:42 +0200

    Update

--- myschoolAPI/users/serializers.py
+++ myschoolAPI/users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User,Announcement
+from .models import User,Announcement ,Message, Post, Notification
 
 class UserSerializer(serializers.ModelSerializer):
         class Meta:
@@ -10,3 +10,18 @@ class AnnouncementSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Announcement
                 fields = ['id', 'department', 'description', 'datecreated', 'image', 'tiltle', 'likes', 'unlikes']
+
+class MessageSerializer(serializers.ModelSerializer):
+        class Meta:
+                model = Message
+                fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+        class Meta:
+                model = Post
+                fields = '__all__'
+
+class NotificationSerializer(serializers.ModelSerializer):
+        class Meta:
+                model = Notification
+                fields = '__all__'
